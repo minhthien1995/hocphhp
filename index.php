629afc4421636515aaf6738a14e560e91ce71703
Thien Tran <tranthien3091995@gmail.com>
2017-07-26 20:19:42 +0700

    desploy project

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+	echo "xin chao";
+	echo "<br>";
+	$a = 5;
+	echo "tong cua $a va $b la $tong";
+	$b = 4;
+	$tong = $a+$b;
+	$demo = array("Thien","Linh","Co");
+	echo "<br>";
+	echo $demo[0];
+
+
+?>
